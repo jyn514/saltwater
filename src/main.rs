123ca31207cfd7643efe28f2edcfc787e98a8dec
Joshua Nelson <jyn514@gmail.com>
2019-05-19 08:33:25 -0400

    allow choosing a filename
    pretty useless now, but will be necessary once we take options and
    multiple files

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,33 @@ extern crate lazy_static;
 mod data;
 mod lex;
 
+use std::env;
+use std::fs::File;
 use std::io::{self, Read};
+use std::process;
 
 use lex::Lexer;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let mut buf = String::new();
-    io::stdin().read_to_string(&mut buf).expect("I/O error reading stdin");
-    for token in Lexer::new("<stdin>", buf) {
+    let filename = if args.len() > 1 {
+        let filename = &args[1];
+        File::open(filename)
+            .and_then(|mut file| file.read_to_string(&mut buf))
+            .unwrap_or_else(|err| {
+                eprintln!("Failed to open {}: {}", args[1], err);
+                process::exit(1);
+            });
+        filename
+    } else {
+        io::stdin().read_to_string(&mut buf).unwrap_or_else(|err| {
+            eprintln!("Failed to read stdin: {}", err);
+            process::exit(1);
+        });
+        "<stdin>"
+    };
+    for token in Lexer::new(&filename, buf.chars()) {
         println!("{:?}", token);
     }
 }

702debe15915479229b679f7b51d5e3441784788
Joshua Nelson <jyn514@gmail.com>
2019-06-10 21:38:16 -0400

    let other people do the hard work of argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,56 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate structopt;
 
 mod data;
 mod lex;
 mod parse;
 mod utils;
 
-use std::env;
 use std::fs::File;
 use std::io::{self, Read};
 use std::process;
+
+use structopt::StructOpt;
 
 use lex::Lexer;
 use parse::Parser;
 use utils::error;
 
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// The file to read C source from.
+    ///
+    /// "-" means stdin (use ./- to read a file called '-').
+    /// Only one file at a time is currently accepted.
+    #[structopt(name = "FILE", default_value = "-", parse(from_str))]
+    filename: String,
+    /// If set, print all tokens found by the lexer in addition to compiling.
+    #[structopt(short = "d", long = "debug-lex")]
+    debug_lex: bool,
+}
+
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    // NOTE: only holds valid UTF-8, and will throw an error on invalid input
+    let opt = Opt::from_args();
+    // NOTE: only holds valid UTF-8; will panic otherwise
     let mut buf = String::new();
-    let filename = if args.len() > 1 && args[1] != "-" {
-        let filename = std::mem::replace(&mut args[1], "".to_string());
-        File::open(&filename)
-            .and_then(|mut file| file.read_to_string(&mut buf))
-            .unwrap_or_else(|err| {
-                eprintln!("Failed to open {}: {}", filename, err);
-                process::exit(1);
-            });
-        filename
-    } else {
+    let filename = if opt.filename == "-" {
         io::stdin().read_to_string(&mut buf).unwrap_or_else(|err| {
             eprintln!("Failed to read stdin: {}", err);
             process::exit(1);
         });
         "<stdin>".to_string()
+    } else {
+        let Opt { filename, .. } = opt;
+        File::open(&filename)
+            .and_then(|mut file| file.read_to_string(&mut buf))
+            .unwrap_or_else(|err| {
+                eprintln!("Failed to read {}: {}", filename, err);
+                process::exit(1);
+            });
+        filename
     };
-    if args.len() > 2 && (args[2] == "-d" || args[2] == "--debug-lex") {
+    if opt.debug_lex {
         for lexeme in Lexer::new(filename.clone(), buf.chars()) {
             match lexeme.data {
                 Ok(l) => println!("{:?}", l),

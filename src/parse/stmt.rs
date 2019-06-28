481be09231216b9a49c039302b3b1a80e85cffbe
Jynn Nelson <github@jyn.dev>
2019-06-27 22:23:03 -0400

    Start on initializers
    - recognize '{' as start of compound statement
    - don't require ';' for function declarations
    - start on compound statements (just '}' for now)

--- src/parse/stmt.rs
+++ src/parse/stmt.rs
@@ -1,9 +1,22 @@
 use super::{Lexeme, Parser};
-use crate::data::{Locatable, Stmt};
+use crate::data::{Locatable, Stmt, Token};
 use std::iter::Iterator;
 
 impl<I: Iterator<Item = Lexeme>> Parser<I> {
     pub fn compound_statement(&mut self) -> Result<Vec<Stmt>, Locatable<String>> {
-        unimplemented!()
+        if self.expect(Token::LeftBrace).is_none() {
+            panic!("compound_statement should be called with '{' as the next token");
+        }
+        let mut stmts = vec![];
+        while self.peek_token() != Some(&Token::RightBrace) {
+            stmts.push(self.statement()?);
+        }
+        if self.expect(Token::RightBrace).is_none() {
+            panic!("peek should always be the same as next");
+        }
+        return Ok(stmts);
+    }
+    pub fn statement(&mut self) -> Result<Stmt, Locatable<String>> {
+        unimplemented!();
     }
 }

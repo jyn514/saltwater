a40563f7fc88be046083a310a8a31c42faab9e8e
Joshua Nelson <jyn514@gmail.com>
2019-07-20 12:37:47 -0400

    Keep track of number of errors
    TODO: instead of using globals, have this per-file,
    which allows running multiple compilers in the same process.
    See https://www.aosabook.org/en/ghc.html under "Crime Doesn't Pay"
    for further discussion.

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,11 @@
-use crate::data::Location;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 use ansi_term::{ANSIString, Colour};
+
+use crate::data::Location;
+
+static WARNINGS: AtomicUsize = AtomicUsize::new(0);
+static ERRORS: AtomicUsize = AtomicUsize::new(0);
 
 fn pretty_print(prefix: ANSIString, msg: &str, location: &Location) {
     eprintln!(
@@ -9,10 +15,20 @@ fn pretty_print(prefix: ANSIString, msg: &str, location: &Location) {
 }
 
 pub fn warn(msg: &str, location: &Location) {
+    WARNINGS.fetch_add(1, Ordering::Relaxed);
     pretty_print(Colour::Yellow.bold().paint("warning"), msg, location);
 }
 pub fn error(msg: &str, location: &Location) {
+    ERRORS.fetch_add(1, Ordering::Relaxed);
     pretty_print(Colour::Red.bold().paint("error"), msg, location);
+}
+
+pub fn get_warnings() -> usize {
+    WARNINGS.load(Ordering::SeqCst)
+}
+
+pub fn get_errors() -> usize {
+    ERRORS.load(Ordering::SeqCst)
 }
 
 /// A simple macro to create a HashMap with minimal fuss.

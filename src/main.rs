ef301ceda81454b3b7a2fff433e3eeb0f1ba7180
Jynn Nelson <github@jyn.dev>
2019-07-20 12:45:46 -0400

    Print number of errors and call llvm::compile

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use std::fs::File;
 use std::io::{self, Read};
 use std::process;
 
+use data::{Declaration, Locatable};
 use lex::Lexer;
 use parse::Parser;
 use utils::error;
@@ -86,11 +87,44 @@ fn main() {
             }
         }
     }
-    let compiler = Parser::new(Lexer::new(filename, buf.chars()));
-    for stmt in compiler {
-        match stmt {
-            Ok(s) => println!("{:#?}", s.data),
-            Err(err) => error(&err.data, &err.location),
-        }
+    let parser = Parser::new(Lexer::new(filename, buf.chars()));
+    let hir = parser.collect::<Result<Vec<Locatable<Declaration>>, Locatable<String>>>();
+    let hir = match hir {
+        Err(err) => err_exit(err),
+        Ok(program) => program,
+    };
+    // why a closure instead of err_exit?
+    // from a conversation in discord#rust-usage:
+    //
+    // A ! value can be coerced into any type implicitly
+    // When you take the function directly you have a value of fn(&'static str) -> ! and that can't be coerced
+    // When you call it you get a value of ! which can
+    // It's like &String can be coerced to &str, but it's not a subtype of it
+    // Likewise a fn(T) -> &String should not be allowed to coerce to fn(T) -> &str
+    //
+    // What's happening here is the function has type `fn(...) -> !`,
+    // but when it's called, that's coerced to `!`,
+    // so the closure has type `fn(...) -> i32`
+    let module = llvm::compile(hir).unwrap_or_else(|e| err_exit(e));
+}
+
+fn err_exit(err: Locatable<String>) -> ! {
+    error(&err.data, &err.location);
+    let (num_warnings, num_errors) = (utils::get_warnings(), utils::get_errors());
+    print_issues(num_warnings, num_errors);
+    process::exit(2);
+}
+
+fn print_issues(warnings: usize, errors: usize) {
+    if warnings == 0 && errors == 0 {
+        return;
     }
+    let warn_msg = if warnings > 1 { "warnings" } else { "warning" };
+    let err_msg = if errors > 1 { "errors" } else { "error" };
+    let msg = match (warnings, errors) {
+        (0, _) => format!("{} {}", errors, err_msg),
+        (_, 0) => format!("{} {}", warnings, warn_msg),
+        (_, _) => format!("{} {} and {} {}", warnings, warn_msg, errors, err_msg),
+    };
+    eprintln!("{} generated", msg);
 }

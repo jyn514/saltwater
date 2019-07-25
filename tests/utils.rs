33fc1bbbad99aee5621fa576476791a890efbf5a
Jynn Nelson <github@jyn.dev>
2019-07-25 12:37:53 -0400

    Give up and use host cc to link
    I don't want to deal with library search paths right now

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,5 +1,19 @@
+use std::io::Error;
+use std::process::{Command, Output};
+
 extern crate compiler;
 
-pub fn compile_program(program: String) {
-    compiler::compile(program, "<integration-test>".to_string(), false);
+use compiler::CompileError;
+
+pub fn compile_and_run(program: String, args: &[&str]) -> Result<Output, CompileError> {
+    compile(program)?;
+    run(args).map_err(CompileError::IO)
+}
+
+pub fn compile(program: String) -> Result<(), CompileError> {
+    compiler::compile_and_assemble(program, "<integration-test>".to_string(), false)
+}
+
+pub fn run(args: &[&str]) -> Result<Output, Error> {
+    Command::new("./a.out").args(args).output()
 }

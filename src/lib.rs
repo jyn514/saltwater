c381d1c5a6d3e3dad9abf9b0ea9c6ee7ceb13707
Joshua Nelson <jyn514@gmail.com>
2019-07-29 12:43:00 -0400

    Don't immediately exit on compile errors
    - Allows using `lib::compile_and_run` and getting errors back
    - Makes `utils` module public so it can be accessed from `main`
    - Add tests for compile errors
    - Return a `Result` from `compile`
    - Add a `utils::fatal` function

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 
 use std::io::{self, Write};
 use std::path::Path;
-use std::process::{self, Command};
+use std::process::Command;
 
 #[macro_use]
 extern crate lazy_static;
@@ -23,7 +23,7 @@ pub use parse::Parser;
 use utils::error;
 
 #[macro_use]
-mod utils;
+pub mod utils;
 mod backend;
 pub mod data;
 mod ir;
@@ -62,7 +62,7 @@ pub fn compile_and_assemble(
 
     let mut tmp_file = NamedTempFile::new().expect("should be able to create temp file");
 
-    let bytes = product.emit().map_err(CompileError::Platform)?;
+    let bytes = product?.emit().map_err(CompileError::Platform)?;
     tmp_file.write_all(&bytes).map_err(CompileError::IO)?;
 
     // link the .o file
@@ -79,7 +79,7 @@ pub fn compile_and_assemble(
     }
 }
 
-pub fn compile(buf: String, filename: String, debug_lex: bool) -> Product {
+pub fn compile(buf: String, filename: String, debug_lex: bool) -> Result<Product, CompileError> {
     if debug_lex {
         for lexeme in Lexer::new(filename.clone(), buf.chars()) {
             match lexeme.data {
@@ -91,41 +91,10 @@ pub fn compile(buf: String, filename: String, debug_lex: bool) -> Product {
     let parser = Parser::new(Lexer::new(filename, buf.chars()));
     let hir = parser.collect::<Result<Vec<Locatable<Declaration>>, Locatable<String>>>();
     let hir = match hir {
-        Err(err) => err_exit(err),
+        Err(err) => return Err(CompileError::Semantic(err)),
         Ok(program) => program,
     };
-    // why a closure instead of err_exit?
-    // from a conversation in discord#rust-usage:
-    //
-    // A ! value can be coerced into any type implicitly
-    // When you take the function directly you have a value of fn(&'static str) -> ! and that can't be coerced
-    // When you call it you get a value of ! which can
-    // It's like &String can be coerced to &str, but it's not a subtype of it
-    // Likewise a fn(T) -> &String should not be allowed to coerce to fn(T) -> &str
-    //
-    // What's happening here is the function has type `fn(...) -> !`,
-    // but when it's called, that's coerced to `!`,
-    // so the closure has type `fn(...) -> i32`
-    ir::compile(hir).unwrap_or_else(|e| err_exit(e)).finish()
-}
-
-fn err_exit(err: Locatable<String>) -> ! {
-    error(&err.data, &err.location);
-    let (num_warnings, num_errors) = (utils::get_warnings(), utils::get_errors());
-    print_issues(num_warnings, num_errors);
-    process::exit(2);
-}
-
-fn print_issues(warnings: usize, errors: usize) {
-    if warnings == 0 && errors == 0 {
-        return;
-    }
-    let warn_msg = if warnings > 1 { "warnings" } else { "warning" };
-    let err_msg = if errors > 1 { "errors" } else { "error" };
-    let msg = match (warnings, errors) {
-        (0, _) => format!("{} {}", errors, err_msg),
-        (_, 0) => format!("{} {}", warnings, warn_msg),
-        (_, _) => format!("{} {} and {} {}", warnings, warn_msg, errors, err_msg),
-    };
-    eprintln!("{} generated", msg);
+    ir::compile(hir)
+        .map_err(CompileError::Semantic)
+        .map(|product| product.finish())
 }

6698187264506d0e5419f4ad32b1d55ae9acfe13
Joshua Nelson <jyn514@gmail.com>
2019-08-13 09:36:15 -0400

    Add options and require structopt for CLI parsing
    Adds no_link
    Since faerie requires structopt anyway, it doesn't make sense to make it
    optional

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,29 @@
-#[cfg(feature = "better_parsing")]
-extern crate structopt;
-#[cfg(feature = "better_parsing")]
-use structopt::StructOpt;
-
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::PathBuf;
 use std::process;
-#[cfg(not(feature = "better_parsing"))]
-use std::{env, ffi::OsString};
+
+extern crate structopt;
+use structopt::StructOpt;
 
 extern crate compiler;
-use compiler::{compile_and_assemble, utils, CompileError};
+use compiler::{compile_and_assemble, utils, CompileError, Opt};
 
-#[cfg_attr(feature = "better_parsing", derive(StructOpt, Debug))]
-struct Opt {
+#[derive(StructOpt)]
+struct CmdLine {
     /// The file to read C source from.
     ///
     /// "-" means stdin (use ./- to read a file called '-').
     /// Only one file at a time is currently accepted.
-    #[cfg_attr(
-        feature = "better_parsing",
-        structopt(name = "FILE", default_value = "-", parse(from_os_str))
-    )]
+    #[structopt(name = "FILE", default_value = "-", parse(from_os_str))]
     filename: PathBuf,
-    /// If set, print all tokens found by the lexer in addition to compiling.
-    #[cfg_attr(feature = "better_parsing", structopt(short = "d", long = "debug-lex"))]
-    debug_lex: bool,
-    /// The output file to use. Defaults to 'a.out'.
-    #[cfg_attr(
-        feature = "better_parsing",
-        structopt(short = "o", long = "output", default_value = "-", parse(from_os_str))
-    )]
-    output: PathBuf,
+
+    #[structopt(flatten)]
+    options: Opt,
 }
 
 fn main() {
-    #[cfg(feature = "better_parsing")]
-    let opt = Opt::from_args();
-    #[cfg(not(feature = "better_parsing"))]
-    let opt = {
-        let mut args = env::args_os();
-        args.next();
-        let first = args.next().unwrap_or_else(|| OsString::from("-"));
-        let debug = first.to_string_lossy() == "-d";
-        let filename = if debug {
-            PathBuf::from(args.next().unwrap_or_else(|| OsString::from("-")))
-        } else {
-            PathBuf::from(first)
-        };
-        let flag = args
-            .next()
-            .map_or(String::new(), |s| s.to_string_lossy().into_owned());
-        let output = match flag.as_str() {
-            "-o" | "--output" => args
-                .next()
-                .unwrap_or_else(|| panic!("missing argument to -o")),
-            _ => OsString::from("a.out"),
-        };
-        Opt {
-            filename,
-            debug_lex: debug,
-            output: PathBuf::from(output),
-        }
-    };
+    let opt = CmdLine::from_args();
     // NOTE: only holds valid UTF-8; will panic otherwise
     let mut buf = String::new();
     let filename = if opt.filename == PathBuf::from("-") {
@@ -73,14 +33,13 @@ fn main() {
         });
         PathBuf::from("<stdin>")
     } else {
-        let Opt { filename, .. } = opt;
-        File::open(filename.as_path())
+        File::open(opt.filename.as_path())
             .and_then(|mut file| file.read_to_string(&mut buf))
             .unwrap_or_else(|err| {
-                eprintln!("Failed to read {}: {}", filename.to_string_lossy(), err);
+                eprintln!("Failed to read {}: {}", opt.filename.to_string_lossy(), err);
                 process::exit(1);
             });
-        filename
+        opt.filename
     };
     // why a closure instead of err_exit?
     // from a conversation in discord#rust-usage:
@@ -94,13 +53,8 @@ fn main() {
     // What's happening here is the function has type `fn(...) -> !`,
     // but when it's called, that's coerced to `!`,
     // so the closure has type `fn(...) -> i32`
-    compile_and_assemble(
-        buf,
-        filename.to_string_lossy().to_string(),
-        opt.debug_lex,
-        &opt.output,
-    )
-    .unwrap_or_else(|err| err_exit(err));
+    compile_and_assemble(buf, filename.to_string_lossy().to_string(), opt.options)
+        .unwrap_or_else(|err| err_exit(err));
 }
 
 fn err_exit(err: CompileError) -> ! {

2637da1bdcafeca706595c7cb71631e569476a22
Joshua Nelson <jyn514@gmail.com>
2019-08-27 13:44:43 -0400

    fix fuzz test runner

--- fuzz/afl/garbage.rs
+++ fuzz/afl/garbage.rs
@@ -5,7 +5,7 @@ extern crate compiler;
 fn main() {
     fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
-            compiler::compile(s.into(), "<fuzz test>".into(), false, false);
+            compiler::compile(s.into(), "<fuzz test>".into(), false, false, false);
         }
     });
 }

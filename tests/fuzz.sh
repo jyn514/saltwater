7109a89685241725472ebc7b40ef66cbf15ecc02
Joshua Nelson <jyn514@gmail.com>
2019-08-31 19:26:09 -0400

    add fuzz script

---
+++ tests/fuzz.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+if cargo +nightly >/dev/null 2>&1; then
+	CARGO='cargo +nightly'
+elif cargo --version | grep -q nightly; then
+	CARGO='cargo'
+else
+	echo "cargo nightly is not installed, can't fuzz"
+	exit 1
+fi
+
+exists() {
+	which "$1" >/dev/null 2>&1
+}
+
+if ! exists cargo-fuzz; then
+	$CARGO install cargo-fuzz
+fi
+$CARGO fuzz run libfuzzer -- -timeout=1 || true
+
+if ! exists cargo-afl; then
+	cargo install afl
+fi
+cd fuzz
+RUSTFLAGS="-Clink-arg=-fuse-ld=gold" cargo afl build --bin afl
+AFL_SKIP_CPUFREQ=1 timeout 120 cargo afl fuzz -i afl/inputs -o afl/outputs target/debug/afl || true
+cat afl/outputs/crashes/* afl/outputs/hangs/* || true
+cd ..

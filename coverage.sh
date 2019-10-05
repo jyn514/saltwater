cef6c64af8b303f5971a276e2c3bce98fee2b477
Joshua Nelson <jyn514@gmail.com>
2019-10-05 19:38:41 -0400

    Fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cargo test --no-run
-rm target/debug/compiler-*.d
-kcov --verify target/cov target/debug/compiler-*
+set -eu
+rm -f target/debug/*-*
+RUSTFLAGS='-C debuginfo=2' cargo test --no-run
+ls -1 target/debug/*-* \
+	| grep -v '\.d$' \
+	| xargs -n1 kcov --verify target/cov
 xdg-open target/cov/index.html

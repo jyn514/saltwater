2924a6660d8e1781d83e5db82b53bc9af3be22f5
Jynn Nelson <github@jyn.dev>
2019-10-29 22:40:40 -0400

    clean up Scope implementation

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -260,32 +260,24 @@ impl<K: Hash + Eq, V> Scope<K, V> {
         self.0.pop();
     }
     pub fn get(&self, name: &K) -> Option<&V> {
-        debug_assert!(!self.0.is_empty());
-        for map in self.0.iter().rev() {
-            let current = map.get(name);
-            if current.is_some() {
-                return current;
-            }
-        }
-        None
+        self.iter()
+            .filter_map(|(key, value)| if key == name { Some(value) } else { None })
+            .next()
     }
     // returns whether the _immediate_ scope contains `name`
     #[inline]
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        debug_assert!(!self.0.is_empty());
         self.0.last_mut().unwrap().insert(key, value)
     }
     #[inline]
     pub fn get_immediate(&self, name: &K) -> Option<&V> {
-        debug_assert!(!self.0.is_empty());
         self.0.last().unwrap().get(name)
     }
     #[inline]
     pub fn get_all_immediate(&mut self) -> &mut HashMap<K, V> {
-        debug_assert!(!self.0.is_empty());
         self.0.last_mut().unwrap()
     }
-    #[inline(always)]
+    #[inline]
     pub fn depth(&self) -> usize {
         self.0.len()
     }
@@ -296,6 +288,12 @@ impl<K: Hash + Eq, V> Scope<K, V> {
     pub fn _remove(&mut self, key: &K) -> Option<V> {
         debug_assert!(!self.0.is_empty());
         self.0.last_mut().unwrap().remove(key)
+    }
+    pub fn iter(&self) -> impl Iterator<Item = (&K, &V)> {
+        self.0.iter().rev().flatten()
+    }
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&K, &mut V)> {
+        self.0.iter_mut().rev().flatten()
     }
 }
 

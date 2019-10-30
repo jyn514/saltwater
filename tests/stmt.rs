ed2097504c264e89d2a1a764f1944a0202dfc021
Jynn Nelson <github@jyn.dev>
2019-10-29 23:48:07 -0400

    fix crash in loop with return in else but not if
    closes https://github.com/jyn514/rcc/issues/106
    
    if_has_return was always being set to true, since it came after the
    jump_to_block

--- tests/stmt.rs
+++ tests/stmt.rs
@@ -128,6 +128,20 @@ fn while_loop() {
         while (i) --i;
     }",
     );
+    utils::assert_succeeds(
+        "
+int f(void) {
+    int i;
+    while (0) {
+        if (1)
+            i = 1;
+        else
+            return 1;
+    }
+    return 0;
+}
+int main() { return f(); }",
+    );
 }
 
 #[test]

d0cf4b31856ce9a56335a8637993a4fe401346a4
Jynn Nelson <github@jyn.dev>
2019-12-08 15:14:43 -0500

    Revert "Revert "autogenerate headers from host""
    revert-ception
    
    this branch is going to serve as a canary for
    https://github.com/jyn514/rcc/issues/131 so I know how much progress is
    being made.

--- tests/c-headers/stddef.c
+++
@@ -1,7 +0,0 @@
-typedef long int ptrdiff_t;
-typedef long unsigned int size_t;
-typedef int wchar_t;
-typedef struct {
-  long long __max_align_ll __attribute__((__aligned__(__alignof__(long long))));
-  long double __max_align_ld __attribute__((__aligned__(__alignof__(long double))));
-} max_align_t;

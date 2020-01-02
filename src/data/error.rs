8c13881afbadfaedf08d7007b5507641a627e019
pythondude325 <gischeaffer@gmail.com>
2020-01-01 18:59:26 -0600

    Added tests

--- src/data/error.rs
+++ src/data/error.rs
@@ -9,7 +9,7 @@ pub type CompileResult<T> = Result<T, CompileError>;
 pub type SemanticError = Locatable<String>;
 pub type CompileError = Locatable<Error>;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct ErrorHandler {
     errors: Vec<CompileError>,
 }
@@ -26,15 +26,6 @@ impl ErrorHandler {
     #[must_use]
     pub fn is_successful(&self) -> bool {
         self.errors.is_empty()
-    }
-}
-
-impl<'a> IntoIterator for &'a ErrorHandler {
-    type Item = &'a CompileError;
-    type IntoIter = <&'a Vec<CompileError> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.errors.iter()
     }
 }
 
@@ -160,5 +151,149 @@ impl<T, E: Into<CompileError>> Recover for RecoverableResult<T, Vec<E>> {
             }
             i
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ErrorKind;
+    use crate::data::prelude::*;
+
+    fn new_error(error: Error) -> CompileError {
+        CompileError::new(error, Location::default())
+    }
+
+    #[test]
+    fn test_error_handler_push_err() {
+        let mut error_handler = ErrorHandler::new();
+        error_handler.push_err(new_error(Error::UnterminatedComment));
+
+        assert_eq!(
+            error_handler,
+            ErrorHandler {
+                errors: vec![new_error(Error::UnterminatedComment)]
+            }
+        );
+    }
+
+    #[test]
+    fn test_error_handler_is_successful() {
+        let mut error_handler = ErrorHandler::new();
+        assert!(error_handler.is_successful());
+        error_handler.push_err(new_error(Error::UnterminatedComment));
+        assert!(!error_handler.is_successful());
+    }
+
+    #[test]
+    fn test_error_handler_into_iterator() {
+        let mut error_handler = ErrorHandler::new();
+        error_handler.push_err(new_error(Error::GenericSemantic("stuff".to_string())));
+        let errors = error_handler.into_iter().collect::<Vec<_>>();
+        assert_eq!(errors.len(), 1);
+    }
+
+    #[test]
+    fn test_error_kind() {
+        assert_eq!(Error::GenericLex("".to_string()).kind(), ErrorKind::Lex);
+        assert_eq!(
+            Error::GenericSemantic("".to_string()).kind(),
+            ErrorKind::Semantic
+        );
+        assert_eq!(
+            Error::GenericSyntax("".to_string()).kind(),
+            ErrorKind::Syntax
+        );
+        assert_eq!(Error::UnterminatedComment.kind(), ErrorKind::Lex);
+    }
+
+    #[test]
+    fn test_compile_error_semantic() {
+        assert_eq!(
+            CompileError::semantic(Locatable::new("".to_string(), Location::default())),
+            new_error(Error::GenericSemantic("".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_compile_error_is_kind() {
+        let e = new_error(Error::GenericLex("".to_string()));
+        assert!(e.is_lex_err());
+        assert!(!e.is_semantic_err());
+        assert!(!e.is_syntax_err());
+
+        let e = new_error(Error::GenericSemantic("".to_string()));
+        assert!(!e.is_lex_err());
+        assert!(e.is_semantic_err());
+        assert!(!e.is_syntax_err());
+
+        let e = new_error(Error::GenericSyntax("".to_string()));
+        assert!(!e.is_lex_err());
+        assert!(!e.is_semantic_err());
+        assert!(e.is_syntax_err());
+    }
+
+    #[test]
+    fn test_compile_error_display() {
+        assert_eq!(
+            format!("{}", new_error(Error::UnterminatedComment)),
+            "invalid token: unterminated /* comment"
+        );
+
+        assert_eq!(
+            format!(
+                "{}",
+                new_error(Error::GenericSemantic("bad code".to_string()))
+            ),
+            "invalid program: bad code"
+        );
+    }
+
+    #[test]
+    fn test_compile_error_from_locatable_string() {
+        let e = CompileError::from(Locatable::new("apples".to_string(), Location::default()));
+        assert_eq!(e, new_error(Error::GenericSemantic("apples".to_string())));
+    }
+
+    #[test]
+    fn test_compile_error_from_syntax_error() {
+        let e = CompileError::from(SyntaxError(Locatable::new(
+            "oranges".to_string(),
+            Location::default(),
+        )));
+        assert_eq!(e, new_error(Error::GenericSyntax("oranges".to_string())));
+    }
+
+    #[test]
+    fn test_recover_error() {
+        let mut error_handler = ErrorHandler::new();
+        let r: RecoverableResult<i32> = Ok(1);
+        assert_eq!(r.recover(&mut error_handler), 1);
+        assert!(error_handler.is_successful());
+
+        let mut error_handler = ErrorHandler::new();
+        let r: RecoverableResult<i32> = Err((new_error(Error::UnterminatedComment), 42));
+        assert_eq!(r.recover(&mut error_handler), 42);
+        let errors = error_handler.into_iter().collect::<Vec<_>>();
+        assert_eq!(errors, vec![new_error(Error::UnterminatedComment)]);
+    }
+
+    #[test]
+    fn test_recover_multiple_errors() {
+        let mut error_handler = ErrorHandler::new();
+        let r: RecoverableResult<i32, Vec<CompileError>> = Ok(1);
+        assert_eq!(r.recover(&mut error_handler), 1);
+        assert!(error_handler.is_successful());
+
+        let mut error_handler = ErrorHandler::new();
+        let r: RecoverableResult<i32, Vec<CompileError>> = Err((
+            vec![
+                new_error(Error::UnterminatedComment),
+                new_error(Error::GenericSemantic("pears".to_string())),
+            ],
+            42,
+        ));
+        assert_eq!(r.recover(&mut error_handler), 42);
+        let errors = error_handler.into_iter().collect::<Vec<_>>();
+        assert_eq!(errors, vec![new_error(Error::UnterminatedComment)]);
     }
 }

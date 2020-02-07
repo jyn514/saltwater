3705af50c6c449e1aab7712c4c8404ca8b0021a5
Joshua Nelson <jyn514@gmail.com>
2020-02-07 14:01:46 -0500

    fix libfuzzer rust program

--- fuzz/fuzz_targets/garbage.rs
+++ fuzz/fuzz_targets/garbage.rs
@@ -4,7 +4,11 @@ extern crate libfuzzer_sys;
 extern crate rcc;
 
 fuzz_target!(|data: &[u8]| {
+    let opt = rcc::Opt {
+        filename: "<fuzz test>".into(),
+        ..Default::default()
+    };
     if let Ok(s) = std::str::from_utf8(data) {
-        rcc::compile(s.into(), "<fuzz test>".into(), false, false, false);
+        rcc::compile(s.into(), &opt);
     }
 });

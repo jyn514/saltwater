70ea2e4191c292ba1e7f807be0118122ea52347f
Jynn Nelson <github@jyn.dev>
2020-02-07 13:46:35 -0500

    remove unneeded paren filter

--- fuzz/afl/garbage.rs
+++ fuzz/afl/garbage.rs
@@ -26,30 +26,10 @@ fn is_exotic_keyword(s: &str) -> bool {
     }
 }
 
-fn too_many_parens(s: &str, n: usize) -> bool {
-    let tokens = lex(s).filter(Result::is_ok);
-    let mut parens_in_a_row = 0;
-    for token in tokens {
-        if let Ok(Locatable {
-            data: Token::LeftParen,
-            ..
-        }) = token
-        {
-            parens_in_a_row += 1;
-            if parens_in_a_row == n {
-                return true;
-            }
-        } else {
-            parens_in_a_row = 0;
-        }
-    }
-    false
-}
-
 fn main() {
     fuzz!(|s: &[u8]| {
         if let Ok(s) = std::str::from_utf8(s) {
-            if !is_exotic_keyword(s) && !too_many_parens(s, 800) {
+            if !is_exotic_keyword(s) {
                 rcc::compile(s, "<fuzz test>".into(), false, false, false);
             }
         }

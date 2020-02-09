b3f95df736833cf670deddacda3d2d2f1484961f
Joshua Nelson <jyn514@gmail.com>
2020-02-09 15:03:40 -0500

    fix runner tests

--- tests/runner-tests/factorial.c
+++ tests/runner-tests/factorial.c
@@ -1,15 +1,16 @@
+// no-main
 extern int factorial_recursive(int n) {
     if (n < 2) {
         return 1;
     } else {
-        return n * factorial(n - 1);
+        return n * factorial_recursive(n - 1);
     }
 }
 
 extern int factorial_iterative(int n) {
-    int n = 1;
+    int sum = 1;
     while (n >= 2) {
-        n *= factorial(n - 1);
+        sum *= n--;
     }
     return 1;
 }

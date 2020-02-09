c6de3970df098159f3acad6e7ad7410ee54b2bf9
Joshua Nelson <jyn514@gmail.com>
2020-02-09 14:48:02 -0500

    add runner tests

---
+++ benches/examples.rs
@@ -0,0 +1,30 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rcc::compile;
+
+const FACTORIAL: &str = include_str!("../tests/runner-tests/factorial.c");
+const FIBONACCI: &str = include_str!("../tests/runner-tests/fibonacci.c");
+
+fn examples(c: &mut Criterion) {
+    let opts = rcc::Opt {
+        filename: "<benchmark>".into(),
+        ..Default::default()
+    };
+    let mut group = c.benchmark_group("Fibonacci");
+    group.bench_function("rcc", |b| {
+        b.iter(|| black_box(compile(FIBONACCI, &opts)));
+    });
+    group.finish();
+
+    let mut group = c.benchmark_group("Factorial");
+    group.bench_function("rcc", |b| {
+        b.iter(|| black_box(compile(FACTORIAL, &opts)));
+    });
+    group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = examples
+}
+criterion_main!(benches);

31213affb2b10a1c839d6a6fb37f31bf1affe9fe
Joshua Nelson <jyn514@gmail.com>
2020-02-09 16:52:57 -0500

    remove unused script

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-rm -f target/debug/*-*
-RUSTFLAGS='-C debuginfo=2' cargo test --no-run
-ls -1 target/debug/*-* \
-	| grep -v '\.d$' \
-	| xargs -n1 kcov --verify target/cov
-xdg-open target/cov/index.html

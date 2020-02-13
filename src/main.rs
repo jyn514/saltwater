2ba1a561585eb7edbbc370c1dfeb8e87789ff1c5
Adel Prokurov <adel.prokurov@protonmail.com>
2020-02-13 10:01:54 +0300

    Fix Clippy error on CString

--- src/main.rs
+++ src/main.rs
@@ -123,10 +123,13 @@ fn real_main(
             let args = std::env::args();
             let argc = args.len() as i32;
             let vec_args = args
-                .into_iter()
-                .map(|string| std::ffi::CString::new(string).unwrap().as_ptr() as *const u8)
-                .collect::<Vec<*const u8>>();
-            let pointer = vec_args.as_ptr() as *const *const u8;
+                .map(|string| std::ffi::CString::new(string).unwrap())
+                .collect::<Vec<std::ffi::CString>>();
+            let pointer = vec_args
+                .iter()
+                .map(|cstr| cstr.as_ptr() as *const u8)
+                .collect::<Vec<*const u8>>()
+                .as_ptr() as *const *const u8;
             let main: unsafe extern "C" fn(i32, *const *const u8) -> i32 =
                 unsafe { std::mem::transmute(main_function) };
             let _ = unsafe { main(argc, pointer) };

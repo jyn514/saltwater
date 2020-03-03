1b7c8f5270d1df040fa90385a28533132b08c6b1
Jynn Nelson <github@jyn.dev>
2020-03-02 23:34:44 -0500

    Remove files from new pratt-parser
    These snuck in somehow

--- parser/src/decl.rs
+++
@@ -1,8 +0,0 @@
-use super::*;
-use crate::data::ast::Declaration;
-
-impl<I: Iterator<Item = Lexeme>> Parser<I> {
-    pub fn declaration(&mut self) -> SyntaxResult<Locatable<Declaration>> {
-        unimplemented!()
-    }
-}

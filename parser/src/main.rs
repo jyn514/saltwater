e81af0c7b2e286603821bf43172260cd6f39acb9
Jynn Nelson <github@jyn.dev>
2020-03-02 23:54:51 -0500

    add missing files

---
+++ parser/src/main.rs
@@ -0,0 +1,16 @@
+use parser::{Lexer, Parser};
+use std::io::Read;
+
+fn main() {
+    let mut src = String::new();
+    std::io::stdin().read_to_string(&mut src).unwrap();
+    let mut lexer = Lexer::new((), src, false);
+    let first = lexer.next().unwrap().unwrap();
+    let mut parser = Parser::new(first, lexer, false);
+    println!("{}", parser.expr().unwrap());
+    /*
+    for decl in parser {
+        println!("{}", decl.unwrap().data);
+    }
+    */
+}

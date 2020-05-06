8b93bd7010922f6bc8e18857b070dddeb7e6b1f8
pythondude325 <me@gisch.dev>
2020-05-05 22:17:08 -0500

    Fixed anon struct newlines

--- src/data/types.rs
+++ src/data/types.rs
@@ -282,7 +282,7 @@ fn write_struct_type(struct_type: &StructType, f: &mut Formatter) -> fmt::Result
             write!(f, "{}", name)?;
         }
         StructType::Anonymous(members) => {
-            write!(f, "{{")?;
+            writeln!(f, "{{")?;
             for member in members.iter() {
                 writeln!(f, "    {};", member)?;
             }
@@ -481,7 +481,7 @@ pub(crate) mod tests {
 
     #[test]
     fn test_big_one() {
-        assert_decl_display("struct { int i; } S;", "struct {    int i;\n} S;");
+        assert_decl_display("struct { int i; } S;", "struct {\n    int i;\n} S;");
         assert_decl_display("int f();", "int f();");
         assert_decl_display("int bar;", "int bar;");
         assert_decl_display("int *foo;", "int *foo;");

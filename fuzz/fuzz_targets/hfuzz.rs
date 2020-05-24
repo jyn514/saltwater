c70c9623976035f2d1227e05a0e22c4d00833dbd
Kevin Becker <k.becker@byter.dev>
2020-05-24 20:07:24 +0200

    remove the keyword check, as they're being ignored internally now

--- fuzz/fuzz_targets/hfuzz.rs
+++ fuzz/fuzz_targets/hfuzz.rs
@@ -2,26 +2,7 @@
 extern crate honggfuzz;
 extern crate rcc;
 
-use rcc::Locatable;
-use rcc::data::lex::{Keyword::*, Token};
-use rcc::PreProcessorBuilder;
 use std::default::Default;
-
-fn is_exotic_keyword(s: &str) -> bool {
-    let (first, _) = PreProcessorBuilder::new(s).build().first_token();
-    let first = match first {
-        Some(Locatable {
-            data: Token::Keyword(k),
-            ..
-        }) => k,
-        _ => return false,
-    };
-    match first {
-        Restrict | Complex | Atomic | Imaginary | ThreadLocal | Generic | StaticAssert
-        | Alignof | Alignas => true,
-        _ => false,
-    }
-}
 
 fn main() {
     use rcc::Opt;
@@ -29,11 +10,9 @@ fn main() {
     loop {
         fuzz!(|s: &[u8]| {
             if let Ok(s) = std::str::from_utf8(s) {
-                if !is_exotic_keyword(s) {
-                    let module = rcc::initialize_aot_module("<test-suite>".into());
-                    let opt = Opt::default();
-                    let _ = rcc::compile(module, s, opt);
-                }
+                let module = rcc::initialize_aot_module("<test-suite>".into());
+                let opt = Opt::default();
+                let _ = rcc::compile(module, s, opt);
             }
         });
     }

a41312146c13f07fc9dcf9763d9859b233ed419a
Joshua Nelson <jyn514@gmail.com>
2020-07-24 10:21:13 -0400

    Misc improvements
    - Move va_list to stdarg.h
    - Give _slightly_ better error message when using an unsupported macro

--- headers/stddef.h
+++ headers/stddef.h
@@ -7,10 +7,7 @@ typedef unsigned long size_t;
 typedef long max_align_t;
 // Unicode is at most 32-bits per character
 typedef int wchar_t;
-typedef __builtin_va_list va_list;
-// glibc is awful and I hate it
-typedef __builtin_va_list __gnuc_va_list;
 
 #define NULL 0
-#define offsetof(type, member) (saltwater does not currently support offsetof)
+#define offsetof(type, member) (offsetof not_currently_supported)
 #endif

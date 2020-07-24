81d31439841c8a6771bc69d7fc092b43b3d08b33
Joshua Nelson <jyn514@gmail.com>
2020-07-23 23:54:37 -0400

    saltwater can run hello world!!!

--- headers/stddef.h
+++ headers/stddef.h
@@ -1,1 +1,11 @@
+// http://port70.net/~nsz/c/c11/n1570.html#7.19
+typedef long ptrdiff_t;
+typedef unsigned long size_t;
+typedef long max_align_t;
+// Unicode is at most 32-bits per character
+typedef int wchar_t;
+// glibc is awful and I hate it
+typedef va_list __gnuc_va_list;
+
 #define NULL 0
+#define offsetof(type, member) (saltwater does not currently support offsetof)

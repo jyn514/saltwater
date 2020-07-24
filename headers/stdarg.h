3fa1d574d3347bf05e816bf8c4b0b5b29821aa68
Joshua Nelson <jyn514@gmail.com>
2020-07-23 23:59:19 -0400

    Add missing stdarg.h

---
+++ headers/stdarg.h
@@ -0,0 +1,5 @@
+// va_list is a built-in type
+#define va_arg(ap, type) (va_arg is not currently supported by saltwater)
+#define va_copy(ap, type) (va_copy is not currently supported by saltwater)
+#define va_end(ap, type) (va_end is not currently supported by saltwater)
+#define va_start(ap, type) (va_start is not currently supported by saltwater)
